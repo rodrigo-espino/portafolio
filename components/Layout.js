2520d8aa2ebcaccbe36426866748a33fee1312db
Rodrigo Espino <rodrigosantacruz42@gmail.com>
2021-10-27 21:28:55 -0500

    error-Linkedin

--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,7 @@ const Layout = ({ children }) => (
     <div className="social-bar">
 		<a href="https://github.com/rodrigo-espino" className="icon icon-github" target="_blank"></a>
 		<a href="https://twitter.com/rodrigo_se_mx" className="icon icon-twitter" target="_blank"></a>
-		<a href="www.linkedin.com/in/rodrigo-se-mx" className="icon icon-linkedin" target="_blank"></a>
+		<a href="https://www.linkedin.com/in/rodrigo-se-mx" className="icon icon-linkedin" target="_blank"></a>
 		<a href="mailto:rodrigosantacruz42@gmail.com" className="icon icon-mail" target="_blank"></a>
 		<a href="https://t.me/rodrigo_espino" className="icon icon-telegram" target="_blank"></a>
 	</div>
