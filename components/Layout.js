1a31736fa9557a0ed55458b5423f803ab49a337d
Rodrigo Espino <rodrigosantacruz42@gmail.com>
2021-10-27 21:21:55 -0500

    social-media

--- components/Layout.js
+++ components/Layout.js
@@ -12,9 +12,9 @@ const Layout = ({ children }) => (
     <Navbar></Navbar>
     <div className="social-bar">
 		<a href="https://github.com/rodrigo-espino" className="icon icon-github" target="_blank"></a>
-		<a href="https://twitter.com/gerberdemenudo" className="icon icon-twitter" target="_blank"></a>
-		<a href="https://www.linkedin.com/in/rodrigo-santacruz-espino-03b8a6194/" className="icon icon-linkedin" target="_blank"></a>
-		<a href="mailto:rodrigo@midesc.com" className="icon icon-mail" target="_blank"></a>
+		<a href="https://twitter.com/rodrigo_se_mx" className="icon icon-twitter" target="_blank"></a>
+		<a href="www.linkedin.com/in/rodrigo-se-mx" className="icon icon-linkedin" target="_blank"></a>
+		<a href="mailto:rodrigosantacruz42@gmail.com" className="icon icon-mail" target="_blank"></a>
 		<a href="https://t.me/rodrigo_espino" className="icon icon-telegram" target="_blank"></a>
 	</div>
     <main className="container py-4">
